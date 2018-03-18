0e419486b0cf33bd05a971ef3c40b3b41aaef1cc
Szymon K <19334989+szy54@users.noreply.github.com>
2018-03-18 01:24:33 +0100

    generate, copy, sort working

--- cw02/zad1/main.c
+++ cw02/zad1/main.c
@@ -10,20 +10,112 @@ void generate (char* fileName, int recordsNum, int recordLength){
     int f = open(fileName, O_WRONLY|O_CREAT|O_TRUNC);
     unsigned char *buffer =malloc(recordLength);
     int i,j;
-    for(i=0;i<recordsNum;i++){
+    for(i=0;i<recordsNum-1;i++){
         for(j=0;j<recordLength;j++){
             buffer[j]=65+rand()%26;    
         }
+        buffer[recordLength-1]='\n';
         printf("%s\n", buffer);
         write(f, buffer, recordLength);
-        write(f, "\n", 1);
+        //write(f, "\n", 1);
     }
     close(f);
 }
 
-void makeActions(int argc, char* argv[]){
-    generate (argv[1], atoi(argv[2]), atoi(argv[3]));
+int bufferCompare(char *buff1, char *buff2){
+    unsigned char a = buff1[0];
+    unsigned char b = buff2[0];
+    if(a<b) return -1;
+    else if(a==b) return 0;
+    else return 1;
+}
+
+void sort(char *fileName, int recordsNum, int recordLength, int sys){
+    char *buffer1 = malloc(recordLength);
+    char *buffer2 = malloc(recordLength);
+    if(sys==1){
+        int f=open(fileName, O_RDWR);
+        int i,j;
+        for(i=1;i<recordsNum;i++){
+            lseek(f, i*recordLength, SEEK_SET);
+            read(f, buffer1, recordLength);
+            j=i-1;
+            while(j>=0){
+                //printf("%d ", j);
+                lseek(f, j*recordLength, SEEK_SET);
+                read(f, buffer2, recordLength);
+                if(bufferCompare(buffer2, buffer1)==1){
+                    lseek(f, (j+1)*recordLength, SEEK_SET);
+                    write(f, buffer2, recordLength);
+                    j=j-1;
+                    lseek(f, (j+1)*recordLength, SEEK_SET);
+                    write(f, buffer1, recordLength);
+                }
+                else{
+                    break;
+                }
+            }
+            printf("%d sorted\n", i);
+        }
+        close(f);
+    }
+    else{
+        FILE *f=fopen(fileName, "r+");
+        int i,j;
+        for(i=1;i<recordsNum;i++){
+            fseek(f, i*recordLength, 0);
+            fread(buffer1, sizeof(char), recordLength, f);
+            j=i-1;
+            while(j>=0){
+                fseek(f, j*recordLength, 0);
+                fread(buffer2, sizeof(char), recordLength, f);
+                if(bufferCompare(buffer2, buffer1)==1){
+                    fseek(f, (j+1)*recordLength, 0);
+                    fwrite(buffer2, sizeof(char), recordLength, f);
+                    j=j-1;
+                    fseek(f, (j+1)*recordLength, 0);
+                    fwrite(buffer1, sizeof(char), recordLength, f);
+                }
+                else{
+                    break;
+                }
+
+            }
+            printf("%d sorted\n", i);
+        }
+        fclose(f);
+    }
     
+}
+
+void copy (char *srcFile, char *destFile, int recordsNum, int recordLength, int sys){  //sys=1 -> system functions, otherwise lib functions
+    int bytes;
+    char *buffer = malloc(recordLength);
+
+    if(sys==1){
+        int fIn=open(srcFile, O_RDONLY);
+        int fOut=open(destFile, O_WRONLY|O_CREAT|O_TRUNC);
+        while((bytes=read(fIn, buffer, recordLength))>0){
+            write(fOut, buffer, bytes);
+        }
+        close(fIn);
+        close(fOut);
+    }
+    else{
+        FILE *fIn=fopen(srcFile, "r");
+        FILE *fOut=fopen(destFile, "w");
+        while((bytes=fread(buffer, sizeof(char), recordLength, fIn))>0){
+            fwrite(buffer, sizeof(char), bytes, fOut);
+        }
+        fclose(fIn);
+        fclose(fOut);
+    }
+}
+
+void makeActions(int argc, char* argv[]){
+    //generate (argv[1], atoi(argv[2]), atoi(argv[3]));
+    //copy(argv[1], argv[2], atoi(argv[3]), atoi(argv[4]), atoi(argv[5]));
+    sort(argv[1], atoi(argv[2]), atoi(argv[3]), atoi(argv[4]));
 }
 
 
