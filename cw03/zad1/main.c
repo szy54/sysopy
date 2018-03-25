27ef3642960bed8847c146a9729d4ae7ba78cc47
Szymon K <19334989+szy54@users.noreply.github.com>
2018-03-25 21:32:21 +0200

    changes to zad1, zad2 complete

---
+++ cw03/zad1/main.c
@@ -0,0 +1,122 @@
+#define _XOPEN_SOURCE 700
+#include <time.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <pwd.h>
+#include <string.h>
+#include <limits.h>
+#include <ftw.h>
+
+
+const char format[] = "%Y-%m-%d %H:%M:%S";
+char buffer[PATH_MAX];
+
+double date_compare(time_t date_1, time_t date_2) {
+    return difftime(date_1, date_2);
+}
+
+void printDetails(char *path, struct stat *file_stat){
+    printf(" %lld\t", file_stat->st_size);
+    printf((file_stat->st_mode & S_IRUSR) ? "r" : "-");
+    printf((file_stat->st_mode & S_IWUSR) ? "w" : "-");
+    printf((file_stat->st_mode & S_IXUSR) ? "x" : "-");
+    printf((file_stat->st_mode & S_IRGRP) ? "r" : "-");
+    printf((file_stat->st_mode & S_IWGRP) ? "w" : "-");
+    printf((file_stat->st_mode & S_IXGRP) ? "x" : "-");
+    printf((file_stat->st_mode & S_IROTH) ? "r" : "-");
+    printf((file_stat->st_mode & S_IWOTH) ? "w" : "-");
+    printf((file_stat->st_mode & S_IXOTH) ? "x" : "-");
+    strftime(buffer, PATH_MAX, format, localtime(&file_stat->st_mtime));
+    printf(" %s\t", buffer);
+    printf(" %s\t", path);
+    printf("\n");
+}
+
+
+void fileInfoGrabber (char * path, char *operant, time_t date){
+    if (path == NULL)
+        return;
+    DIR *dir = opendir(path);
+
+
+    if (dir == NULL) {
+        printf("%s\n", "error!");
+        return;
+    }
+
+    struct dirent *rdir = readdir(dir);
+    struct stat file_stat;
+
+    char new_path[PATH_MAX];
+
+    while (rdir != NULL) {
+        strcpy(new_path, path);
+        strcat(new_path, "/");
+        strcat(new_path, rdir->d_name);
+
+
+        lstat(new_path, &file_stat); // with symlinks
+
+        if (strcmp(rdir->d_name, ".") == 0 || strcmp(rdir->d_name, "..") == 0) {
+            rdir = readdir(dir);
+            continue;
+        } else {
+            if (S_ISREG(file_stat.st_mode)) {
+                if (strcmp(operant, "=") == 0 && date_compare(date, file_stat.st_mtime) == 0) {
+                    printDetails(new_path, &file_stat);
+                } else if (strcmp(operant, "<") == 0 && date_compare(date, file_stat.st_mtime) > 0) {
+                    printDetails(new_path, &file_stat);
+                } else if (strcmp(operant, ">") == 0 && date_compare(date, file_stat.st_mtime) < 0) {
+                    printDetails(new_path, &file_stat);
+                }
+            }
+
+
+            if (S_ISDIR(file_stat.st_mode)) {
+                pid_t pid = fork();
+                if(pid==0){
+                    fileInfoGrabber(new_path, operant, date);
+                    exit(0);
+                }
+
+            }
+            rdir = readdir(dir);
+        }
+    }
+    closedir(dir);
+}
+
+int main (int argc, char *argv[]){
+    //argv[1] - sciezka
+    //argv[2] - < > =
+    //argv[3] - data
+    if(argc<4){
+        printf("too few arguments");
+    }
+
+    char *path=argv[1];
+    char *sign=argv[2];
+    char *usrDate=argv[3];
+
+    printf("%s %s %s\n", path, sign, usrDate);
+    struct tm *tm=malloc(sizeof(struct tm));
+    strptime(usrDate, format, tm);
+    time_t date = mktime(tm);
+    
+    DIR *dir = opendir(realpath(path, NULL));
+    if(dir ==NULL){
+        printf("couldn't open the dir\n");
+        return 1;
+    }
+    fileInfoGrabber(realpath(path, NULL), sign, date);
+
+    closedir(dir);
+
+
+    return 0;
+
+}
---
+++ cw03/zad2/main.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+#define MAXARGC 50
+#define WHITE " "
+
+int parsecommand(char * cmdstr)
+{
+    char *cmdstrdup = strdup(cmdstr);
+    if(cmdstrdup == NULL)
+      //insuficient memory, do some errorhandling. 
+      return 0; 
+    char *saveptr;
+    char *ptr;
+    char *argv[MAXARGC]; 
+    int argc;
+
+    if((ptr = strtok_r(cmdstrdup, WHITE, &saveptr)) == NULL)
+    {
+        printf("%s\n", "no args given");
+        return 0;
+    } 
+
+    argv[argc = 0] = cmdstrdup;
+    while(ptr != NULL) {
+        //printf("%d\n", argc);
+        ptr = strtok_r(NULL, WHITE, &saveptr);
+        if(++argc >= MAXARGC-1) // -1 for room for NULL at the end
+            break;
+        argv[argc] = ptr;
+    }
+    /*
+    int i;
+    for(i=0;i<argc;i++){
+        printf("%s\n", argv[i]);
+    }
+    */
+    int exitState;
+    pid_t childPid = fork();
+    if(childPid==0){
+        execvp(argv[0], argv);
+        exit(17);
+    }
+    else{
+        //printf("%d %d", childPid, exitState);
+        //waitpid(childPid, &exitState, 0);
+        waitpid(childPid, &exitState, 0);
+        //printf("The status is %d\n", exitState);
+        if(WIFEXITED(exitState) && WEXITSTATUS(exitState)==0){
+            printf("\n");
+            free(cmdstrdup);
+            return 1;
+        } 
+        else{
+            printf("Error: process did not exited with 0!\n"); //maybe I should put some status macro here to get the err msg
+            free(cmdstrdup);
+            return 0;
+        }
+    }
+    //printf("%d", argc);
+}
+
+void linesReading(char *fileName){
+    FILE *fp;
+    char *line = NULL;
+    size_t len =0;
+    ssize_t read;
+
+    fp=fopen(fileName, "r");
+    if(fp==NULL)
+        exit(EXIT_FAILURE);
+    while((read=getline(&line, &len, fp))!=-1){
+        //printf("retrieved line of length %zu:\n", read);
+        //printf("%s", line);
+        if(line[read-1]=='\n')line[read-1]=NULL;
+        if(parsecommand(line)!=1)
+            break;
+
+    }
+    free(line);
+    fclose(fp);
+}
+
+
+int main(int argc, char* argv[]){
+    linesReading(argv[1]);
+    return 0;
+}
